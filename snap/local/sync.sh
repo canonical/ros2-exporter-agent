f190c3592478afae9e756c03c42d9f8acf9ce282
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2024-02-20 17:52:49 +0100

    address comments

--- snap/local/sync.sh
+++ snap/local/sync.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash -e
 
+STORAGE_PATH="$(snapctl get storage-path)"
+
 ROBOT_ID=$(cat $SNAP_COMMON/robot_id.txt)
 
-STORAGE_PATH="~/$ROBOT_ID"
-
-snapctl set storage-path=$STORAGE_PATH
+if [ -n "$ROBOT_ID" ]; then
+    STORAGE_PATH="~/$ROBOT_ID"
+    snapctl set storage-path=$STORAGE_PATH
+fi
 
 rsync -avz -e "ssh -F $SNAP_USER_COMMON/.ssh/config" --min-size=1 $SNAP_COMMON/ storage-server:$STORAGE_PATH
