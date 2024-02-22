558d6ce3685bc0b1b1026f6337ff8afaad5baeb5
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2024-02-22 14:55:25 +0100

    use device id

--- snap/local/sync.sh
+++ snap/local/sync.sh
@@ -2,11 +2,13 @@
 
 STORAGE_PATH="$(snapctl get storage-base-path)"
 
-ROBOT_ID=$(cat $SNAP_COMMON/robot_id.txt)
+DEVICE_ID=$(cat $SNAP_COMMON/device_id.txt)
 
-if [ -n "$ROBOT_ID" ]; then
-    STORAGE_PATH="${STORAGE_PATH%/}/$ROBOT_ID"
+if [ -n "$DEVICE_ID" ]; then
+    STORAGE_PATH="${STORAGE_PATH%/}/$DEVICE_ID"
     snapctl set storage-path=$STORAGE_PATH
+else
+    echo "DEVICE_ID is not set. Make sure it's available in the rob-cos-data-sharing snap."
 fi
 
 rsync -avz -e "ssh -F $SNAP_USER_COMMON/.ssh/config" --min-size=1 $SNAP_COMMON/ storage-server:$STORAGE_PATH
