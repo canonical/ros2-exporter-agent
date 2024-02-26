b90712f2de55a4d1ae8a1200f50ecfbe2c781def
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2024-02-26 14:24:05 +0100

    read ssh keys from common

--- snap/local/sync.sh
+++ snap/local/sync.sh
@@ -11,4 +11,29 @@ else
     echo "DEVICE_ID is not set. Make sure it's available in the rob-cos-data-sharing snap."
 fi
 
+if [ ! -d $SNAP_USER_COMMON/.ssh ]; then
+    mkdir $SNAP_USER_COMMON/.ssh
+fi
+
+if [ -f $SNAP_COMMON/device_private_key ]; then
+    cp $SNAP_COMMON/device_private_key  $SNAP_USER_COMMON/.ssh/
+else
+    echo "could not find private_key"
+fi
+if [ -f $SNAP_COMMON/device_public_key.pub ]; then
+    cp $SNAP_COMMON/device_public_key.pub  $SNAP_USER_COMMON/.ssh/
+else
+    echo "could not find public key"
+fi
+
+cat > $SNAP_USER_COMMON/.ssh/config <<EOF
+Host storage-server
+    User ubuntu
+    HostName ubuntu
+    IdentityFile /root/snap/ros2-exporter-agent/common/.ssh/device_private_key
+    UserKnownHostsFile /root/snap/ros2-exporter-agent/common/.ssh/known_hosts
+EOF
+
+echo "SSH config file and keys setup completed."
+
 rsync -avz -e "ssh -F $SNAP_USER_COMMON/.ssh/config" --min-size=1 $SNAP_COMMON/ storage-server:$STORAGE_PATH
