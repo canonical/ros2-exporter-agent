359d3ca0e16343e52ad020d08e4ec80459d9e24a
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2024-03-07 14:13:50 +0100

    use snap_data for now, use write interface because we have to modify the permissions of the private_rsa_key

--- snap/local/sync.sh
+++ snap/local/sync.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash -e
 
 STORAGE_PATH="$(snapctl get storage-base-path)"
-
-DEVICE_ID=$(cat $SNAP_COMMON/device_id.txt)
+REMOTE_SERVER_IP="$(snapctl get remote-server-ip)"
+REMOTE_SERVER_PORT="$(snapctl get remote-server-port)"
+DEVICE_ID=$(cat $SNAP_DATA/device_id.txt)
 
 if [ -n "$DEVICE_ID" ]; then
     STORAGE_PATH="${STORAGE_PATH%/}/$DEVICE_ID"
@@ -11,29 +12,21 @@ else
     >&2 echo "DEVICE_ID is not set. Make sure it's available in the rob-cos-data-sharing snap."
 fi
 
-if [ ! -d $SNAP_USER_COMMON/.ssh ]; then
-    mkdir $SNAP_USER_COMMON/.ssh
+if [ ! -f $SNAP_DATA/device_rsa_key.pub ]; then
+     >&2 echo "could not find device_rsa_key.pub. Make sure it's available in the rob-cos-data-sharing snap."
+else
+   chmod 600 $SNAP_DATA/device_rsa_key
 fi
 
-if [ -f $SNAP_COMMON/device_rsa_key ]; then
-    cp $SNAP_COMMON/device_rsa_key  $SNAP_USER_COMMON/.ssh/
-else
-    >&2 echo "could not find device_rsa_key"
-fi
-if [ -f $SNAP_COMMON/device_rsa_key.pub ]; then
-    cp $SNAP_COMMON/device_rsa_key.pub  $SNAP_USER_COMMON/.ssh/
-else
-    >&2 echo "could not find device_rsa_key.pub"
-fi
-
-cat > $SNAP_USER_COMMON/.ssh/config <<EOF
+cat > $SNAP_USER_COMMON/config <<EOF
 Host storage-server
     User root
-    HostName 10.166.108.243
-    IdentityFile /root/snap/ros2-exporter-agent/common/.ssh/device_rsa_key
-    UserKnownHostsFile /root/snap/ros2-exporter-agent/common/.ssh/known_hosts
+    HostName $REMOTE_SERVER_IP
+    StrictHostKeyChecking no
+    UserKnownHostsFile=/dev/null
+    IdentityFile $SNAP_DATA/device_rsa_key
 EOF
 
 echo "SSH config file and keys setup completed."
 
-rsync -avz -e "ssh -F $SNAP_USER_COMMON/.ssh/config -p 2222" --min-size=1 $SNAP_COMMON/ storage-server:/var/lib/caddy-fileserver/$STORAGE_PATH
+rsync -avz -e "ssh -F $SNAP_USER_COMMON/config -p 2222" --min-size=1 $SNAP_COMMON/data/ storage-server:$STORAGE_PATH
