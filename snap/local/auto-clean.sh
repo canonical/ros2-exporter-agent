4440cee44404ae6e6533c40a7ec6079140c437a3
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2024-04-15 17:12:09 +0200

    fix(ros2-exporter-agent): verbose rm

--- snap/local/auto-clean.sh
+++ snap/local/auto-clean.sh
@@ -6,12 +6,12 @@ echo "Starting auto-clean."
 LIST_OF_FILES=$(fdfind --type file --change-older-than 10min . $SNAP_COMMON/data) # TODO make the time configurable
 
 # remove old files
-rm -f $LIST_OF_FILES
+rm -f --verbose $LIST_OF_FILES
 
 # get the list of empty directories
 LIST_OF_EMPTY_DIRS=$(fdfind --type directory --type empty . $SNAP_COMMON/data)
 
 # remove old an empty directories
-rm -df $LIST_OF_EMPTY_DIRS
+rm -df --verbose $LIST_OF_EMPTY_DIRS
 
 echo "Auto-clean done."
